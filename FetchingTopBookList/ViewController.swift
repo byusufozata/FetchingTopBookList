c04ba3a0611b45aa0014aa5d181e5f62b325bddd
YUSUF ÖZATA <byusufozata@gmail.com>
2022-12-29 23:10:09 +0300

    Initial Commit

---
+++ FetchingTopBookList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchingTopBookList
+//
+//  Created by YUSUF ÖZATA on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
